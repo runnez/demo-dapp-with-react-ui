e4d3b67db5e1adf7f8a0039452502759d05a3a83
Andrey Boldyrev <runnez@yandex.ru>
2024-02-14 19:08:21 +0300

    reinit

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,58 @@ import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
 import {Footer} from "./components/Footer/Footer";
 import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
+import { useState } from 'react';
+
+const botName = localStorage.getItem('botName') || 'demo_payments_bot';
+const BotNameConfig = () => {
+  const [val, setVal] = useState(botName);
+  const save = () => {
+    localStorage.setItem('botName', val);
+    window.location.reload();
+  }
+  return (
+    <>
+      <input type="text" value={val} onChange={e => setVal(e.target.value)}/>
+      <br />
+      <button onClick={save}>Update</button>
+    </>
+  )
+}
+
 
 function App() {
-  return (
-      <TonConnectUIProvider
-          manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
-          uiPreferences={{ theme: THEME.DARK }}
-          walletsListConfiguration={{
-            includeWallets: [
-              {
-                appName: "tonwallet",
-                name: "TON Wallet",
-                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
-                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
-                universalLink: "https://wallet.ton.org/ton-connect",
-                jsBridgeKey: "tonwallet",
-                bridgeUrl: "https://bridge.tonapi.io/bridge",
-                platforms: ["chrome", "android"]
-              }
-            ]
-          }}
-          actionsConfiguration={{
-              twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
-          }}
-      >
+  return <>
+    <BotNameConfig />
+    <TonConnectUIProvider
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+      uiPreferences={{ theme: THEME.LIGHT }}
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            appName: "telegram-wallet",
+            name: "Wallet",
+            imageUrl: "https://wallet.tg/images/logo-288.png",
+            aboutUrl: "https://wallet.tg/",
+            universalLink: `https://t.me/${botName}/start`,
+            bridgeUrl: "https://bridge.tonapi.io/bridge",
+            platforms: ["ios", "android", "macos", "windows", "linux"]
+          },
+          {
+            appName: "tonwallet",
+            name: "TON Wallet",
+            imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+            aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+            universalLink: "https://wallet.ton.org/ton-connect",
+            jsBridgeKey: "tonwallet",
+            bridgeUrl: "https://bridge.tonapi.io/bridge",
+            platforms: ["chrome", "android"]
+          }
+        ]
+      }}
+      actionsConfiguration={{
+          twaReturnUrl: 'https://t.me/tc_demo_dapp_bot/dapp'
+      }}
+    >
         <div className="app">
             <Header />
             <TxForm />
@@ -35,7 +63,7 @@ function App() {
             <Footer />
         </div>
       </TonConnectUIProvider>
-  )
+    </>
 }
 
 export default App
